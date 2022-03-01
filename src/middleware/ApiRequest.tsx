f14b09a1a45a42fdbb3d134b2c9153d57d1b75f6
Andrew Doyle <ad120695@gmail.com>
2022-03-01 08:25:26 -0500

    Reorganize Files

---
+++ src/middleware/ApiRequest.tsx
@@ -0,0 +1,42 @@
+const API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY; //apikey
+const url = "https://api.unsplash.com/search";
+
+//API query 
+async function pullFromSite(search, pages) {
+  try {
+    const res = await fetch(urlBuilder(search, pages));
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+    return '';
+  }
+}
+
+//returns 10 random images for the landing page
+export async function getDefaults() {
+    try {
+        const res = await fetch('https://api.unsplash.com/photos/random?count=10&client_id='+API_KEY);
+        const data = await res.json();
+        return data;
+    }
+    catch(err) {
+        console.log(err);
+        return null;
+    }
+}
+
+//returns the API requested data to the frontend
+export default function getPhotos(search: string, pages: number) {
+  const photoMap = pullFromSite(search, pages);
+  return photoMap;
+}
+
+//format - https://api.unsplash.com/search?per_page=20&page=1&query="{searchTerm}"
+//creates the URL for the API request
+function urlBuilder(search: string, pageNum: number) {
+  const new_url =
+    url + "?per_page=20&page=" + pageNum + "&query=" + search + "&client_id=" + API_KEY;
+  return new_url;
+}
+
